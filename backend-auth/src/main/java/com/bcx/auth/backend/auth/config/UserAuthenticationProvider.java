2f509c42f269370b24a727a2f3229482376e4b21
kieva290@gmail.com <kieva290@gmail.com>
2024-02-14 22:12:26 +0200

    commit backend-auth modue changes

---
+++ backend-auth/src/main/java/com/bcx/auth/backend/auth/config/UserAuthenticationProvider.java
@@ -0,0 +1,57 @@
+package com.bcx.auth.backend.auth.config;
+
+import com.bcx.auth.backend.auth.entities.AuthUser;
+import com.bcx.auth.backend.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.nio.CharBuffer;
+import java.util.Collections;
+import java.util.Optional;
+
+/**
+ * Provider which will validate the Authentication object present in the SecurityContext,
+ * the only acceptable Authentication object is the UsernamePasswordAuthenticationToken which comes from the,
+ * UserAuthenticationConverter, then from the username and password present in the Authentication object,
+ * validate the information against the database
+ * if the username and password don't match with the data present in the database, null is returned as the
+ * Authentication object
+ */
+@Component
+@RequiredArgsConstructor
+public class UserAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String login = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        Optional<AuthUser> oUser = userRepository.findByLogin(login);
+
+        if (oUser.isEmpty()) {
+            return null;
+        }
+
+        AuthUser authUser = oUser.get();
+
+        if (passwordEncoder.matches(CharBuffer.wrap(password), authUser.getPassword())) {
+            return UsernamePasswordAuthenticationToken.authenticated(login, password, Collections.emptyList());
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.equals(authentication);
+    }
+
+}
---
+++ backend-auth/src/main/java/com/bcx/auth/backend/auth/entities/AuthUser.java
@@ -0,0 +1,42 @@
+package com.bcx.auth.backend.auth.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(
+        name = "auth_user"
+)
+public class AuthUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_auth_generator")
+    @GenericGenerator(
+            name = "sequence_auth_generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "service_auth.auth_user_id_seq"),
+                    @Parameter(name = "initial_value", value = "1000"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+
+    private Long id;
+
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+}
